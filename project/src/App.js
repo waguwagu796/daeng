7ad537173609734ee84e3370e99aa344d0602d24
waguwagu796 <bbosa1012@gmail.com>
2025-10-22 15:51:08 +0900

    다시 수정

--- project/src/App.js
+++ project/src/App.js
@@ -9,7 +9,7 @@ function App() {
       <main className="main-content">
         <div className="hero-section">
           <h1>지도는</h1>
-          <p>여기에 넣을 예정...</p>
+          <p>여기에 넣을 예정수정</p>
         </div>
       </main>
     </div>
