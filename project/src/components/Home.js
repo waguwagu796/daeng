83dfd8c5729b36be194a36449c771acb941656a3
waguwagu796 <bbosa1012@gmail.com>
2025-10-23 09:13:56 +0900

    메인페이지 조금 수정 및 링크 이동 하는 중

---
+++ project/src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../App.css';
+
+const Home = () => {
+    return (
+        <div>
+            <main className="main-content">
+                <div className="hero-section">
+                    <h1>지도는</h1>
+                    <p>여기에 넣을 예정수정</p>
+                </div>
+            </main>
+        </div>
+    );
+};
+
+export default Home;
